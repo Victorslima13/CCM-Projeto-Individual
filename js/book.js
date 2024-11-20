0d1d3b51d86511523639e16ed5c6c8e4543a61d4
Victorslima13 <victor.slima@sptech.school>
2024-11-20 16:05:06 -0300

    Muita coisa

--- js/book.js
+++ js/book.js
@@ -1,1 +1,37 @@
-function flippage() {}
+const readqty = document.querySelectorAll('.read')
+
+var pageposition = 0
+
+function hidepage() {
+    for (var i = 0; i < readqty.length; i++) {
+        readqty[i].classList.remove('on')
+    }
+}
+
+function showpage() {
+    readqty[pageposition].classList.add('on')
+}
+
+function flippageback() {
+    hidepage()
+
+    if (pageposition === 0) {
+        pageposition = readqty.length - 1
+    } else {
+        pageposition--
+    }
+
+    showpage()
+}
+
+function flippagenext() {
+    hidepage()
+
+    if (pageposition === readqty.length - 1) {
+        pageposition = 0
+    } else {
+        pageposition++
+    }
+
+    showpage()
+}
