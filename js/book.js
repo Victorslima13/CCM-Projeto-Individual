a5289d8f672f644b51460683db40d83d38dafeb7
Victorsilva13 <victor.slima@sptech.school>
2024-11-20 21:47:56 -0300

    conclusão do funcionamento do livro e inicio das paginas da tela principal

--- js/book.js
+++ js/book.js
@@ -1,4 +1,4 @@
-const readqty = document.querySelectorAll('.read')
+const readqty = document.querySelectorAll(".read")
 
 var pageposition = 0
 
@@ -16,20 +16,24 @@ function flippageback() {
     hidepage()
 
     if (pageposition === 0) {
+        document.getElementById("booktitle").style.display = 'flex'
         pageposition = readqty.length - 1
     } else {
+        document.getElementById("booktitle").style.display = 'none'
         pageposition--
     }
-
+    
     showpage()
 }
 
 function flippagenext() {
     hidepage()
-
+    
     if (pageposition === readqty.length - 1) {
+        document.getElementById("booktitle").style.display = 'flex'
         pageposition = 0
     } else {
+        document.getElementById("booktitle").style.display = 'none'
         pageposition++
     }
 
